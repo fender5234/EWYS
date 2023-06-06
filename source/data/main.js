c88062a23f8f9bd0b601c6a648211925aa285546
fender5234 <fender5234@gmail.com>
2023-06-06 13:03:48 +0300

    Добавляет Хэдэр и футер

---
+++ source/data/main.js
@@ -0,0 +1,12 @@
+export default ({ error, pageName, status }) => ({
+	appData: {
+		page: {
+			error,
+			pageName,
+			status
+		}
+	},
+	description: 'My project',
+	pixelperfect: JSON.stringify({ breakpoints: [320,1440], ext: 'webp' }),
+	projectName: 'My app'
+});
