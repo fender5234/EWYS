5df8ee48c588d0c06dfa64b58a15d58f6a11f9ce
fender5234 <fender5234@gmail.com>
2023-06-11 22:57:38 +0300

    адаптив главной продолжение

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -60,3 +60,20 @@ mqs.addEventListener('change', () => {
 		aboutListItem.forEach((item) => item.classList.remove('swiper-slide'));
 	}
 });
+
+// Функция скрыть показать текст перед футером
+const showText = () => {
+	const infoWrapper = document.querySelector('.info__wrapper');
+	const button = document.querySelector('.info__button-text ');
+	const openText = document.querySelector('.info__button-text-show-text');
+	const hideText = document.querySelector('.info__button-text-hide-text');
+
+	button.addEventListener('click', () => {
+		infoWrapper.classList.toggle('info__wrapper--show-text');
+		button.classList.toggle('info__button-text--active');
+		openText.classList.toggle('info__button-text-show-text--hide');
+		hideText.classList.toggle('info__button-text-hide-text--show');
+	});
+};
+showText();
+//
